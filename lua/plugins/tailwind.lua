8ddee7f1dd87b30418d2322823f7b66f0f9055b0
CREEK <41039917+creekdrops@users.noreply.github.com>
2023-12-13 14:26:45 -0600

    added colorizer

--- lua/plugins/tailwind.lua
+++ lua/plugins/tailwind.lua
@@ -1,33 +1,57 @@
 return {
-  "neovim/nvim-lspconfig",
-  opts = {
-    servers = {
-      tailwindcss = {
-        -- exclude a filetype from the default_config
-        filetypes_exclude = { "markdown" },
-        -- add additional filetypes to the default_config
-        filetypes_include = {
-          "svelte",
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        tailwindcss = {
+          -- exclude a filetype from the default_config
+          filetypes_exclude = { "markdown" },
+          -- add additional filetypes to the default_config
+          filetypes_include = {
+            "svelte",
+          },
         },
       },
+      setup = {
+        tailwindcss = function(_, opts)
+          local tw = require("lspconfig.server_configurations.tailwindcss")
+          opts.filetypes = opts.filetypes or {}
+
+          -- Add default filetypes
+          vim.list_extend(opts.filetypes, tw.default_config.filetypes)
+
+          -- Remove excluded filetypes
+          --- @param ft string
+          opts.filetypes = vim.tbl_filter(function(ft)
+            return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
+          end, opts.filetypes)
+
+          -- Add additional filetypes
+          vim.list_extend(opts.filetypes, opts.filetypes_include or {})
+        end,
+      },
     },
-    setup = {
-      tailwindcss = function(_, opts)
-        local tw = require("lspconfig.server_configurations.tailwindcss")
-        opts.filetypes = opts.filetypes or {}
-
-        -- Add default filetypes
-        vim.list_extend(opts.filetypes, tw.default_config.filetypes)
-
-        -- Remove excluded filetypes
-        --- @param ft string
-        opts.filetypes = vim.tbl_filter(function(ft)
-          return not vim.tbl_contains(opts.filetypes_exclude or {}, ft)
-        end, opts.filetypes)
-
-        -- Add additional filetypes
-        vim.list_extend(opts.filetypes, opts.filetypes_include or {})
-      end,
+  },
+  {
+    "NvChad/nvim-colorizer.lua",
+    opts = {
+      user_default_options = {
+        tailwind = true,
+      },
+      filetypes_include = {
+        "svelte",
+      },
     },
+  },
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      { "roobert/tailwindcss-colorizer-cmp.nvim", config = true },
+    },
+    opts = function(_, opts)
+      opts.formatting = {
+        format = require("tailwindcss-colorizer-cmp").formatter,
+      }
+    end,
   },
 }
