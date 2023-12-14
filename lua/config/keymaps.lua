d53a3d71607b95f6d14ac3e7b7268ec6848ba91c
CREEK <41039917+creekdrops@users.noreply.github.com>
2023-12-14 13:25:40 -0600

    vs code style move up and down

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+-- Move line up
+keymap("n", "<A-Up>", ":move -2<CR>", opts)
+keymap("i", "<A-Up>", "<Esc>:move -2<CR>i", opts)
+
+-- Move line down
+keymap("n", "<A-Down>", ":move +1<CR>", opts)
+keymap("i", "<A-Down>", "<Esc>:move +1<CR>i", opts)
